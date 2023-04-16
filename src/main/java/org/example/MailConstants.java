7d86d2d310813e4c933e593745038e7e555f0a97
harshsikarwar20 <harshsikarwar125@gmail.com>
2023-04-16 19:43:03 +0530

    New commit

---
+++ src/main/java/org/example/MailConstants.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class MailConstants {
+
+    public static final String SENDER = "harshsikarwar125@gmail.com";
+
+    // receiver email
+    public static final String RECEIVERMAILADDRESS = "chauhantejasvi47@gmail.com";
+
+    //subject of the mail
+    public static final String SUBJECT = "Testing Java mail : 12-April-2023";
+
+    //message to be sent in the mail body
+    public static final String MESSAGE = "This message is for security check...!!!";
+
+}
